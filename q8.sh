4efe8841ab57eec07da62696cec9d7fb1e786bc6
timkartar <timkartar7879@gmail.com>
2019-10-22 15:52:01 -0700

    return correct contig row from index file for q8

---
+++ q8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+fai=$1  #give fai filename as input
+sort -k2 --numeric-sort --reverse $fai > sorted.fai
+awk '{t+=$2; print t;}' sorted.fai > cummsum.txt
+total=$(tail -1 cummsum.txt)
+idx=0
+#echo $((total / 2))
+while read p; do
+  idx=$((idx + 1))
+  if [ $p -gt $((total / 2)) ]; 
+  then
+#	echo $idx
+	break
+  fi
+  
+done < cummsum.txt
+
+echo the required contig index:
+sed "${idx}q;d" sorted.fai
+
+rm sorted.fai cummsum.txt
