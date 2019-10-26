fc89a8d48a647189a0d4590a3ed1f2f17f141846
Jiawei-Huang <jellyhuang14@gmail.com>
2019-10-25 18:23:30 -0700

    use short scale to plot structural variants and replot SNV density histogram

--- plot_q4.py
+++ plot_q4.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
+#import seaborn as sns
 
 out=sys.argv[2]
 inp = sys.argv[1]
@@ -12,7 +13,11 @@ for i in raw:
     except:
         print(i)
 
-plt.hist(toplot,bins=10000)
-plt.xlim(-17000,17000)
+#plt.hist(toplot,bins=10000)
+#sns.displot(toplot, bins=np.linspace(-2000,2000,100))
+plt.hist(toplot, bins=np.linspace(-2000,2000,100))
+#plt.xlim(-17000,17000)
+plt.xlabel("length of structual variants")
+plt.ylabel("counts of each bin")
 plt.title("distribution of the lengths of structural variants of " + out)
 plt.savefig(out +".q4.png")
