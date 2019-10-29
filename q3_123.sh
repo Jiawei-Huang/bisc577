d759a38e7237b15025b7398ad572efdbe9a34792
Jiawei-Huang <jellyhuang14@gmail.com>
2019-10-28 18:49:19 -0700

    calculate snps

--- q3_123.sh
+++ q3_123.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 f=$1
 echo counts for $f >> Answer_3.txt
-cat $f | awk -v OFS="\t" '$0 !~ "^#" {hom = 0; het = 0; if(substr($10,1,1)== substr($10,3,1) )  hom++;  else het++; print $1, $2, hom, het}' | cut -f3,4 | awk '{x+=$1; y+=$2} END {print "homo:", x,"hetero:", y, "total:", x+y}' >> Answer_3.txt
+grep snp $f | awk -v OFS="\t" '$0 !~ "^#" {hom = 0; het = 0; if(substr($10,1,1)== substr($10,3,1) )  hom++;  else het++; print $1, $2, hom, het}' | cut -f3,4 | awk '{x+=$1; y+=$2} END {print "homo:", x,"hetero:", y, "total:", x+y}' >> Answer_3.txt
--- q3_5.sh
+++ q3_5.sh
@@ -2,7 +2,7 @@
 
 # Find all SNV positions  #Give vcf file name as input
 f=$1
-grep -v "^#" $f |\
+grep -v "^#" $f | grep snp |\
 awk -F '\t' 'BEGIN{printf("create table T(c text,p int); BEGIN TRANSACTION;\n");} {printf("insert into T(c,p) values(\"%s\",%d);\n",$1,$2);} END {printf("COMMIT; SELECT c,CAST(p/2E4  AS INT)*2E4, count(*) from T group by c,CAST(p/2E4  AS INT)*1E7 ; drop table T;");}' |\
 sqlite3 -separator ' ' tmp.sqlite > ${f}.density.txt && rm tmp.sqlite
 
